b511e8ac74d327d0163e65fdd5e672d57b401e7f
arthurgeay <arthurgeay.contact@gmail.com>
2018-05-20 17:44:55 +0200

    Finalisation de l'app

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
+    let activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBAction func changeQuote() {
+        // Sélectionne un élément aléatoire dans le tableau des célébrités
+        let randomIndex1 = Int(arc4random_uniform(UInt32(celebrities.count)))
+        let celebrity = celebrities[randomIndex1]
+        
+        // Sélectionne un élément aléatoire dans le tableau des activités
+        let randomIndex2 = Int(arc4random_uniform(UInt32(activities.count)))
+        let activity = activities[randomIndex2]
+        
+        // Génération de la citation + affectation au label
+        let quote = "Tu es " + celebrity + " " + activity + " !"
+        quoteLabel.text = quote
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
